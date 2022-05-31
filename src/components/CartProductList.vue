464cab10a3eedd3b9eb6d787b31dfd88fe5cbda9
Amir <amva.kzn@mail.ru>
2022-05-31 09:32:57 +0300

    before order

--- src/components/CartProductList.vue
+++ src/components/CartProductList.vue
@@ -1,70 +1,26 @@
 <template>
   <div class="cart-list">
     <h1 v-if="cartProducts.length === 0">Ваша корзина пуста...</h1>
-    <div
-      class="cart-list-item"
+    <CartProductListItem
       v-for="item in cartProducts"
       :key="item.id"
+      :item="item"
       v-else
-    >
-      <img :src="item.image" alt="" class="list-item-image" />
-      <div class="item-inner">
-        <div class="item-title">
-          <p class="item-title-text">{{ item.title }}</p>
-          <button class="delete-button" @click.prevent="deleteProduct(item.id)">
-            x
-          </button>
-        </div>
-        <p class="item-category">Артукул: {{ item.id }}</p>
-        <div class="item-count">
-          <div class="quantity-inner">
-            <button class="bt-minus">-</button>
-            <input type="text" class="quantity" :value="item.amount" />
-            <button class="bt-plus">+</button>
-          </div>
-          <p class="item-total-summ">
-            {{ (item.price * item.amount) | numberFormat }} p
-          </p>
-        </div>
-      </div>
-    </div>
+    />
   </div>
 </template>
 
 <script>
-import numberFormat from "@/helpers/numberFormat";
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters } from "vuex";
+import CartProductListItem from "@/components/CartProductListItem.vue";
 
 export default {
+  components: { CartProductListItem },
   computed: {
     ...mapGetters({
       //получаем товары корзины из глобального хранилища
       cartProducts: "getProducts",
     }),
-  },
-  methods: {
-    ...mapActions({
-      deleteFromCart: "deleteFromCart",
-      loadCart: "loadCart",
-      // updateAmount: "updateAmount",
-    }),
-    deleteProduct(id) {
-      this.deleteFromCart({
-        id: id,
-      }).then(() => {
-        // после удаления из списка, снова получаем корзину
-        this.loadCart();
-      });
-    },
-    // updateAmount() {
-    //   this.$store.commit("updateAmount", {
-    //     id: id.id,
-    //     amount: item.amount,
-    //   });
-    // },
-  },
-  filters: {
-    numberFormat,
   },
 };
 </script>
---
+++ src/components/CartProductListItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="cart-list-item">
+    <img :src="item.image" alt="" class="list-item-image" />
+    <div class="item-inner">
+      <div class="item-title">
+        <p class="item-title-text">{{ item.title }}</p>
+        <button class="delete-button" @click.prevent="deleteProduct(item.id)">
+          x
+        </button>
+      </div>
+      <p class="item-category">Артукул: {{ item.id }}</p>
+      <div class="item-count">
+        <div class="quantity-inner">
+          <button class="bt-minus" @click.prevent="decreaseAmount">-</button>
+          <input type="text" class="quantity" v-model="actualAmount" />
+          <button class="bt-plus" @click.prevent="increaseAmount">+</button>
+        </div>
+        <p class="item-total-summ">
+          {{ (item.price * item.amount) | numberFormat }} p
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import numberFormat from "@/helpers/numberFormat";
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      // количество из корзины записываем сюда
+      actualAmount: this.item.amount,
+    };
+  },
+  props: ["item"],
+  methods: {
+    ...mapActions({
+      updateAmount: "updateAmount",
+      deleteFromCart: "deleteFromCart",
+      loadCart: "loadCart",
+    }),
+    increaseAmount() {
+      this.actualAmount++;
+    },
+    decreaseAmount() {
+      this.actualAmount--;
+    },
+    deleteProduct(id) {
+      this.deleteFromCart({
+        id: id,
+      }).then(() => {
+        // после удаления из списка, снова получаем корзину
+        this.loadCart();
+      });
+    },
+  },
+  watch: {
+    // следим за изменением количества, отправляем запрос put
+    actualAmount(value) {
+      if (this.actualAmount > 0) {
+        this.updateAmount({
+          id: this.item.id,
+          amount: value,
+        });
+      }
+    },
+  },
+  filters: {
+    numberFormat,
+  },
+};
+</script>
